0afa5cb89a4b6a2e0252bf81a2ba5c49218331b4
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-10-22 02:02:20 -0500

    Updated case study data to include all project names, MUI Icon was added into the case study section, and the section will only display the 1st 4 projects.

--- src/Components/Home/CaseStudy.jsx
+++ src/Components/Home/CaseStudy.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import {CaseStudyData} from '../index'
+import EastIcon from '@mui/icons-material/East';
 
 function CaseStudy() {
   return (
     <div className='caseStudy__container'>
-      {CaseStudyData.map((casestudy) => (
-        <div key={casestudy.id} className="individual__study">
-            <img src={casestudy.poster} alt={casestudy.title} />
+        <div className="caseStudy__content px-8">
+            <h2 className='header text-center text-xl my-4'>Our Success Stories</h2>
+            <div className="grid grid-cols-2 gap-4">
+                {CaseStudyData.slice(0, 4).map((casestudy) => (
+                    <div key={casestudy.id} className="individual__study">
+                        <img src={casestudy.poster} alt={casestudy.title} />
+                        <h3 className='header py-2'>{casestudy.name}</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, voluptates consectetur dolores porro iusto molestiae?</p>
+                        <div className="view__caseStudy flex justify-end">
+                            <p className='mr-4'>View More</p>
+                            <EastIcon />
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
-      ))}
     </div>
   )
 }
