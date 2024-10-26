1c96249fbe9b2e1b4f0df6e5893e5bb99012a05f
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-10-26 13:53:45 -0500

    Slider has had some CSS updates such as a gap between slides, and how many slides are displayed at a time.

--- src/Components/Swipers/CaseStudySwiper.jsx
+++ src/Components/Swipers/CaseStudySwiper.jsx
@@ -4,18 +4,30 @@ import CaseStudyData from '../Data/CaseStudyData';  // Import your case study da
 
 const imageSwiperConfig = {
   slidesPerView: 1.25,
+  spaceBetween: 10,
   navigation: false,
   pagination: true,
   loop: true,
+  breakpoints: {
+    1023: {
+        slidesPerView: 2.25,
+    }
+  }
 };
 
 const CaseStudySwiper = () => {
   // Assuming you want to display images from the first case study
   const imageData = CaseStudyData[0].image.map((img) => ({
-    content: <img className="mb-8 border-2" src={img.image} alt={img.alt} />,
+    content: <img className="w-full mb-8 border-2" src={img.image} alt={img.alt} />,
   }));
 
-  return <DynamicSwiper data={imageData} config={imageSwiperConfig} />;
-};
+  return (
+    <div className="slider__parent flex justify-end w-full">
+        <div className="w-full ">
+            <DynamicSwiper data={imageData} config={imageSwiperConfig} />
+        </div>
+    </div>
+  );
+  };
 
 export default CaseStudySwiper;
