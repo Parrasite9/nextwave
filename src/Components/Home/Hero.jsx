42b69c73d5f0c72301d7e1295ef94618e0aa98d1
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-11-02 04:24:11 -0500

    Hero image updated, and fonts have been updated to fit their respective media

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -2,21 +2,21 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='hero-container relative px-4 bg-image-fade'>
+    <div className='hero-container relative px-4'>
       {/* TEXT AND IMAGE SECTION  */}
       <div className="hero grid grid-cols-1 lg:grid-cols-2 gap-4 items-center">
         {/* TEXT SECTION */}
         <div className="text__section text-center md:text-left pt-8">
-          <h3 className='subheader font-bold text-xl py-4 md:text-2xl'>
+          <h3 className='subheader font-bold text-xl py-4 md:text-2xl xl:text-3xl'>
             A New Wave Of Online Success
           </h3>
-          <h1 className='header font-extrabold text-4xl pb-4 md:text-5xl'>
+          <h1 className='header font-extrabold text-4xl pb-4 md:text-5xl xl:text-6xl'>
             Websites That Increase Your Revenue
           </h1>
-          <p className='pb-4'>
+          <p className='pb-4 md:text-xl'>
             Maximize your ROI with websites designed to convert.
           </p>
-          <button className='btn-cta mb-4'>Text Me About A Project</button>
+          <button className='btn-cta mb-4 md:text-2xl md:py-3'>Text Me About A Project</button>
 
           {/* TRUST BADGES */}
           <div className="hidden trustbadges grid grid-cols-2 gap-4 place-items-center
@@ -30,12 +30,12 @@ const Hero = () => {
 
         {/* IMAGE SECTION */}
         <div className="image__section flex justify-center">
-          <img className='w-full h-auto' src="/images/hero/mobile_hero2.jpg" alt="mobile_hero" />
+          <img className='w-full h-auto' src="/images/hero/herosvg.gif" alt="mobile_hero" />
         </div>
       </div>
 
       {/* SVG Wave */}
-      <div className="wave-container absolute bottom-0 left-0 w-full overflow-hidden leading-none z-10">
+      {/* <div className="wave-container absolute bottom-0 left-0 w-full overflow-hidden leading-none z-10">
         <svg
           className="wave-svg relative block w-full h-24"
           viewBox="0 0 1440 320"
@@ -46,7 +46,7 @@ const Hero = () => {
             d="M0,224L48,224C96,224,192,224,288,234.7C384,245,480,267,576,245.3C672,224,768,160,864,149.3C960,139,1056,181,1152,186.7C1248,192,1344,160,1392,144L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
           ></path>
         </svg>
-      </div>
+      </div> */}
 
     </div>
   )
