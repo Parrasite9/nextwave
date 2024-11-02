5806fd406eaa9582fc5c9d196eb12b8bb610a585
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-11-02 03:47:35 -0500

    hero section has svg added for making a wavy portion at the end of the section

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='hero-container px-4'>
+    <div className='hero-container relative px-4 bg-image-fade'>
       {/* TEXT AND IMAGE SECTION  */}
       <div className="hero grid grid-cols-1 lg:grid-cols-2 gap-4 items-center">
         {/* TEXT SECTION */}
@@ -34,6 +34,19 @@ const Hero = () => {
         </div>
       </div>
 
+      {/* SVG Wave */}
+      <div className="wave-container absolute bottom-0 left-0 w-full overflow-hidden leading-none z-10">
+        <svg
+          className="wave-svg relative block w-full h-24"
+          viewBox="0 0 1440 320"
+          preserveAspectRatio="none"
+        >
+          <path
+            fill="#ffffff"
+            d="M0,224L48,224C96,224,192,224,288,234.7C384,245,480,267,576,245.3C672,224,768,160,864,149.3C960,139,1056,181,1152,186.7C1248,192,1344,160,1392,144L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+          ></path>
+        </svg>
+      </div>
 
     </div>
   )
