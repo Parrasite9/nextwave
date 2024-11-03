88b97a5a58cbd2a02794d8cd39866dbaf5250b3d
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-11-03 17:37:18 -0600

    CSS updates have been made to the services section, images (svgs) have been imported, and some of the structure of the file has been updated

--- src/Components/Home/Services.jsx
+++ src/Components/Home/Services.jsx
@@ -2,27 +2,56 @@ import React from 'react'
 
 function Services() {
   return (
-    <div className='services__container grid grid-cols-2 lg:grid-cols-4 gap-4 px-8'>
-      <div className="service border border-2 border-wbite rounded">
-        <img src="" alt="" />
-        <h2>Web Design</h2>
-        <p>Creating visually compelling and user-friendly designs that captivate audiences and enhance user experience.</p>
-      </div>
-      <div className="service border border-2 border-wbite rounded">
-        <img src="" alt="" />
-        <h2>Development</h2>
-        <p>Building robust, scalable websites tailored to your unique business needs, using the latest web technologies.</p>
-      </div>
-      <div className="service border border-2 border-wbite rounded bg-test2 text-white">
-        <img src="" alt="" />
-        <h2 className=''>SEO</h2>
-        <p>Helping your business get found with SEO and targeted digital marketing strategies that drive traffic and boost visibility.</p>
-      </div>
-      <div className="service border border-2 border-wbite rounded">
-        <img src="" alt="" />
-        <h2>Maintnance & Support</h2>
-        <p>Ensuring your website stays secure, up-to-date, and performs flawlessly with ongoing support and maintenance services.</p>
-      </div>
+    <div className='services__container px-8'>
+
+        <h2 className='text-yale-blue text-lg font-semiBold pb-2'>Services</h2>
+
+        <div className="services__info grid grid-cols-2 lg:grid-cols-4 gap-4">
+
+            {/* DESIGN SERVICE  */}
+            <div className="service">
+                <div className="service__image__container pb-4">
+                    <img className='w-full h-auto' src="/images/services/design.svg" alt="design" />
+                </div>
+                <div className="service__text-container bg-lemon-chiffon rounded-xl p-2">
+                    <h2 className='text-yale-blue text-lg font-semiBold pb-2'>Web Design</h2>
+                    <p>Creating visually compelling and user-friendly designs that captivate audiences and enhance user experience.</p>
+                </div>
+            </div>
+
+            {/* DEVELOPMENT SERVICE  */}
+            <div className="service">
+                <div className="service__image__container pb-4">
+                    <img className='w-full h-auto' src="/images/services/landingPage.svg" alt="development" />
+                </div>
+                <div className="service__text-container bg-lemon-chiffon rounded-xl p-2">
+                    <h2 className='text-yale-blue text-lg font-semiBold pb-2'>Development</h2>
+                    <p>Building robust, scalable websites tailored to your unique business needs, using the latest web technologies.</p>
+                </div>
+            </div>
+
+            {/* SEO SERVICE  */}
+            <div className="service">
+                <div className="service__image__container pb-4">
+                    <img className='w-full h-auto' src="/images/services/onlineStats.svg" alt="SEO" />
+                </div>
+                <div className="service__text-container bg-lemon-chiffon rounded-xl p-2">
+                    <h2 className='text-yale-blue text-lg font-semiBold pb-2'>SEO</h2>
+                    <p>Helping your business get found with SEO and targeted digital marketing strategies that drive traffic and boost visibility.</p>
+                </div>
+            </div>
+
+            {/* SUPPORT SERVICE  */}
+            <div className="service">
+                <div className="service__image__container pb-4">
+                    <img className='w-full h-auto' src="/images/services/support.svg" alt="support" />
+                </div>
+                <div className="service__text-container bg-lemon-chiffon rounded-xl p-2">
+                    <h2 className='text-yale-blue text-lg font-semiBold pb-2'>Maintnance & Support</h2>
+                    <p>Ensuring your website stays secure, up-to-date, and performs flawlessly with ongoing support and maintenance services.</p>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
