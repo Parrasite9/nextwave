c55b34c597db7d31e3714037dcd774033b5fe230
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-11-04 13:21:01 -0600

    Services section updated to include a header. Header styles have been updated in the tailwind css file

--- src/Components/Home/Services/ServiceSection.jsx
+++ src/Components/Home/Services/ServiceSection.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Services, Services2 } from '../..'
+import { FaServicestack } from 'react-icons/fa';
 
 function ServiceSection() {
   return (
     <div>
-      <Services2 />
-      <Services />
+        <div className="divider">
+            <div className="divider-line"></div>
+            <span className='divider-text'>Our Services</span>
+            <div className="divider-line"></div>
+        </div>
+
+        <Services2 />
+        <Services />
     </div>
   )
 }
