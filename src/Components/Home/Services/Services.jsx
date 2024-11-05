f1fda3855716f9a9ede909c5f3131f0446df7f3f
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-11-05 00:31:04 -0600

    button style updates, 2xl media query has been created here

--- src/Components/Home/Services/Services.jsx
+++ src/Components/Home/Services/Services.jsx
@@ -72,9 +72,9 @@ const Services = () => {
           <div className="services__button justify-center items-center mb-5">
             <button
               className="bg-blue-500 text-white mt-5 py-2 rounded-md flex justify-center w-full
-                md:w-2/5 md:py-3"
+                 md:py-3 lg:justify-around lg:px-12 xl:w-3/5 2xl:w-1/2"
             >
-              <span className="leading-none">Learn More</span>
+              <span className="leading-none">Free Google Business Insights</span>
               <ArrowForwardIosIcon fontSize="small" />
             </button>
           </div>
--- src/Components/Home/Services/Services2.jsx
+++ src/Components/Home/Services/Services2.jsx
@@ -66,7 +66,7 @@ const Services2 = () => {
           <div className="services2__button justify-center items-center mb-5">
             <button
               className="bg-blue-500 text-white mt-5 py-2 rounded-md flex justify-center w-full
-                md:w-2/5 md:py-3"
+                md:w-4/5 md:py-3 lg:justify-around lg:px-4 lg:w-3/5 xl:w-2/5 xl:px-4 2xl:w-1/2 2xl:px-12"
             >
               <span className="leading-none">No Pressure Zoom Call</span>
               <ArrowForwardIosIcon fontSize="small" />
