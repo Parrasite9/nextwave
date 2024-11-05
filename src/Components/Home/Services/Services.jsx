3361099f812addb09fcc5c38081c5e63c7708ce6
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-11-05 00:55:46 -0600

    Icon updates and CSS updates to the services section

--- src/Components/Home/Services/Services.jsx
+++ src/Components/Home/Services/Services.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
-import MenuBookIcon from '@mui/icons-material/MenuBook';
-import SupportAgentIcon from '@mui/icons-material/SupportAgent';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ScreenSearchDesktopIcon from '@mui/icons-material/ScreenSearchDesktop';
+import InsightsIcon from '@mui/icons-material/Insights';
 
 const Services = () => {
   return (
     <div className="services__container">
       <div className="grid grid-cols-1 md:grid-cols-2 px-8">
         {/* Text Container */}
-        <div className="order-1 py-4 md:px-8 md:py-8">
-          <h3 className="mt-10 text-sky-blue font-semibold xl:text-xl">
+        <div className="order-1 flex flex-col justify-center py-4 md:px-8 md:py-8">
+          <h3 className="mt-10 text-bright-teal font-semibold xl:text-xl">
             Online Presence & Visibility
           </h3>
-          <h2 className="font-bold text-darkGreen text-2xl mb-8 xl:text-3xl">
+          <h2 className="font-bold text-2xl mb-8 xl:text-3xl">
             Optimizing Your Reach for Maximum Impact
           </h2>
           <p className="mb-10">
@@ -26,8 +26,8 @@ const Services = () => {
           <div className="grid grid-cols-5 grid-rows-2 gap-0 xl:flex">
             {/* Icon 1 */}
             <div className="row-start-1 col-start-1 flex items-center justify-center">
-              <div className="bg-vBrightOrange rounded-full flex items-center justify-center w-16 h-16">
-                <MenuBookIcon
+              <div className="bg-yale-blue rounded-full flex items-center justify-center w-16 h-16">
+                <ScreenSearchDesktopIcon
                   className="text-white text-2xl"
                   style={{ width: '30px', height: '30px' }}
                 />
@@ -37,7 +37,7 @@ const Services = () => {
             {/* Text 1 */}
             <div className="row-start-1 col-start-2 col-end-6 flex items-center">
               <div className="services__icon__text p-5">
-                <h2 className="font-semibold mb-2">Search Engine Optimization</h2>
+                <h2 className="font-semibold mb-2 text-bright-teal">Search Engine Optimization</h2>
                 <p>
                   Boost your visibility on search engines with targeted keywords, on-page
                   optimization, and link-building strategies designed to attract organic
@@ -48,8 +48,8 @@ const Services = () => {
 
             {/* Icon 2 */}
             <div className="row-start-2 col-start-1 flex items-center justify-center">
-              <div className="bg-vBrightOrange rounded-full flex items-center justify-center w-16 h-16">
-                <SupportAgentIcon
+              <div className="bg-yale-blue rounded-full flex items-center justify-center w-16 h-16">
+                <InsightsIcon
                   className="text-white"
                   style={{ width: '35px', height: '35px' }}
                 />
@@ -59,7 +59,7 @@ const Services = () => {
             {/* Text 2 */}
             <div className="row-start-2 col-start-2 col-end-6 flex items-center">
               <div className="services__icon__text p-5">
-                <h2 className="font-semibold mb-2">Analytics & Insights</h2>
+                <h2 className="font-semibold mb-2 text-bright-teal">Analytics & Insights</h2>
                 <p>
                   Leverage Google Analytics to gain insights into user behavior, optimize
                   engagement, and measure the performance of your digital strategies in
@@ -71,7 +71,7 @@ const Services = () => {
 
           <div className="services__button justify-center items-center mb-5">
             <button
-              className="bg-blue-500 text-white mt-5 py-2 rounded-md flex justify-center w-full
+              className="btn-primary text-white mt-5 py-2 rounded-md flex justify-center w-full
                  md:py-3 lg:justify-around lg:px-12 xl:w-3/5 2xl:w-1/2"
             >
               <span className="leading-none">Free Google Business Insights</span>
--- src/Components/Home/Services/Services2.jsx
+++ src/Components/Home/Services/Services2.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import MenuBookIcon from '@mui/icons-material/MenuBook';
-import SupportAgentIcon from '@mui/icons-material/SupportAgent';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import GroupAddIcon from '@mui/icons-material/GroupAdd';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
 
 const Services2 = () => {
   return (
     <div className="services2__container bg-lemon-chiffon">
       <div className="grid grid-cols-1 md:grid-cols-2 px-8">
         {/* Text Container */}
-        <div className="order-1 md:order-2 py-4 md:px-8 md:py-8">
+        <div className="order-1 flex flex-col justify-center md:order-2 py-4 md:px-8 md:py-8">
           <h3 className="mt-10 text-bright-teal font-semibold xl:text-xl">
             Web Design & Development
           </h3>
@@ -24,8 +24,8 @@ const Services2 = () => {
           <div className="grid grid-cols-5 grid-rows-2 gap-0 xl:flex">
             {/* Icon 1 */}
             <div className="row-start-1 col-start-1 flex items-center justify-center">
-              <div className="bg-vBrightOrange rounded-full flex items-center justify-center w-16 h-16">
-                <MenuBookIcon
+              <div className="bg-yale-blue rounded-full flex items-center justify-center w-16 h-16">
+                <GroupAddIcon
                   className="text-white text-2xl"
                   style={{ width: '30px', height: '30px' }}
                 />
@@ -44,8 +44,8 @@ const Services2 = () => {
 
             {/* Icon 2 */}
             <div className="row-start-2 col-start-1 flex items-center justify-center">
-              <div className="bg-vBrightOrange rounded-full flex items-center justify-center w-16 h-16">
-                <SupportAgentIcon
+              <div className="bg-yale-blue rounded-full flex items-center justify-center w-16 h-16">
+                <ManageAccountsIcon
                   className="text-white"
                   style={{ width: '35px', height: '35px' }}
                 />
@@ -65,7 +65,7 @@ const Services2 = () => {
 
           <div className="services2__button justify-center items-center mb-5">
             <button
-              className="bg-blue-500 text-white mt-5 py-2 rounded-md flex justify-center w-full
+              className="btn-primary text-white mt-5 py-2 rounded-md flex justify-center w-full
                 md:w-4/5 md:py-3 lg:justify-around lg:px-4 lg:w-3/5 xl:w-2/5 xl:px-4 2xl:w-1/2 2xl:px-12"
             >
               <span className="leading-none">No Pressure Zoom Call</span>
