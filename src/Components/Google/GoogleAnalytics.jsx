d60e383112f9fc22ab806abb0949c5f501bc72cf
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-11-10 03:33:30 -0600

    Cleanup, All Google Analytic buttons for the home page has been completed

--- src/Components/Google/GoogleAnalytics.jsx
+++ src/Components/Google/GoogleAnalytics.jsx
@@ -1,24 +1,3 @@
-// import { useEffect } from 'react';
-// import { useLocation } from 'react-router-dom';
-
-// function GoogleAnalytics() {
-//   const location = useLocation();
-
-//   useEffect(() => {
-//     window.dataLayer = window.dataLayer || [];
-//     window.dataLayer.push({
-//       event: 'page_view',
-//       page_path: location.pathname,
-//       page_location: window.location.href,
-//       page_title: document.title,
-//     });
-//   }, [location]);
-
-//   return null;
-// }
-
-// export default GoogleAnalytics;
-
 // //THIS IS A WORKING VERSION
 
 // GoogleAnalytics.jsx
