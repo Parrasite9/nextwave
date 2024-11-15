0f8e72bcac521a2f844b6f18e4ecc3f65670a00c
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-11-14 19:05:36 -0600

    This is a working version of the code for the data to be accessible globally.

--- src/Pages/CaseStudyDetails.jsx
+++ src/Pages/CaseStudyDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import CaseStudyContext from '../Components/Data/CaseStudyContext';
 import { HelmetProvider, Helmet } from 'react-helmet-async';
