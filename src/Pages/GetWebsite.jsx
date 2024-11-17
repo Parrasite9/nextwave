8bcf0975b0575429b732042811380e5d364bb52f
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-11-17 05:00:25 -0600

    WORKING VERSION: This is a workign version and styling has been completed here

--- src/Pages/GetWebsite.jsx
+++ src/Pages/GetWebsite.jsx
@@ -9,7 +9,7 @@ function GetWebsite() {
   };
 
   return (
-    <div className="max-w-5xl mx-auto p-4">
+    <div className="min-h-screen pt-4">
       {/* Page Header */}
       <h1 className="text-center text-4xl font-bold py-8">Let’s Update Your Information</h1>
       
