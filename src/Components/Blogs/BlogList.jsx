b82fe1f522902ed7f19c645d3799114bdc50de3d
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-11-23 23:23:22 -0600

    Made CSS updates to the BlogList

--- src/Components/Blogs/BlogList.jsx
+++ src/Components/Blogs/BlogList.jsx
@@ -11,17 +11,19 @@ const BlogList = () => {
     }, []);
   
     return (
-      <div>
-        <h1>Our Blog</h1>
+      <div className="px-8">
+        <h1 className="header text-center text-2xl mb-4">Our Blog</h1>
+        <div className="list__of__blogs grid grid-cols-2 gap-4 mb-8 lg:grid-cols-4">
         {blogs.map((blog) => (
-          <div key={blog.id} className="blog-preview">
-            <h2>{blog.title}</h2>
-            <p>By {blog.author} on {blog.date}</p>
+          <div key={blog.id} className="blog-preview flex flex-col justify-between border-4 p-2 cursor-pointer">
+            <h2 className="font-bold">{blog.title}</h2>
+            {/* <p>By {blog.author} on {blog.date}</p> */}
             {/* Use blog.content.introduction instead */}
             <p>{blog.content.introduction.substring(0, 100)}...</p>
-            <Link to={`/blog/${generateSlug(blog.title)}`}>Read More</Link>
+            <Link className="text-sky-blue" to={`/blog/${generateSlug(blog.title)}`}>Read More</Link>
           </div>
         ))}
+        </div>
       </div>
     );
   };
