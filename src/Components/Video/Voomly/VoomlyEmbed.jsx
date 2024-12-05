491db096f087e0d5684bd91f6c17962b9f517405
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-12-05 13:57:02 -0600

    updated voomly embed to accept emails

--- src/Components/Video/Voomly/VoomlyEmbed.jsx
+++ src/Components/Video/Voomly/VoomlyEmbed.jsx
@@ -11,17 +11,11 @@ const VoomlyEmbed = () => {
 		>
 			<iframe
 				src="https://embed.voomly.com/embed/assets/embed.html?videoId=IYiDnbXNi&amp;videoRatio=1.7777777777777777&amp;type=f&amp;skinColor=%23008EFF"
-				frameBorder="0"
+				frameborder="0"
 				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-				allowFullScreen
-				style={{
-					width: '100%',
-					height: '100%',
-					position: 'absolute',
-					top: 0,
-					left: 0,
-				}}
-				title="Voomly Video"
+				allowfullscreen=""
+				width="100%"
+				height="100%"
 			></iframe>
 		</div>
 	);
