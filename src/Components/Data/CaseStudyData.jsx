7fc6298ca787b5280218af62cdbc82cea220d452
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2025-08-13 10:46:27 -0500

    Removed the slug line from the json data and boilerplate as it was not needed there.

--- src/Components/Data/CaseStudyData.jsx
+++ src/Components/Data/CaseStudyData.jsx
@@ -1,149 +1,140 @@
-// TEMPLATE
 // {
-//     poster: '',
-// lgPoster: '/images/casestudy/123/456.webp',//
-// image: [
-//         { image: '' },
-//         { image: '' },
-//         { image: '' },
-//         { image: '' },
-//     ],
-//     colors: {
-//         primary: '',
-//         secondary: '',
-//         ctaButton: '',
-//         ctaButtonText: '',
-//     },
-//     seo: '',
-//     name: '',
-//     shortDescription: '',
-//     timeline: '',
-//     slug: generateSlug(''),
-//     title: '',
-//     teaser: '',
-//     price: '',
-//     intro: '',
-//     role: '',
-//     hasVideo: false,
-//     videoPath: '',
-//     problem: {
-//         header: '',
-//         headerDetail: '',
-//         subheaders: [
-//             {
-//                 subheader: '',
-//                 subHeaderDetail: '',
-//             },
-//             {
-//                 subheader: '',
-//                 subHeaderDetail: '',
-//             },
-//         ],
-//     },
-
-//     goal: {
-//         header: '',
-//         headerDetail: '',
-//         subheaders: [
-//             {
-//                 subheader: '',
-//                 subHeaderDetail: '',
-//             },
-//             {
-//                 subheader: '',
-//                 subHeaderDetail: '',
-//             },
-//             {
-//                 subheader: '',
-//                 subHeaderDetail: '',
-//             },
-//         ],
-//     },
-
-//     impact: {
-//         header: '',
-//         headerDetail: '',
-//         subheaders: [
-//             {
-//                 subheader: '',
-//                 subHeaderDetail: '',
-//             },
-//             {
-//                 subheader: '',
-//                 subHeaderDetail: '',
-//             },
-//         ],
-//     },
-
-//     ideation: {
-//         header: '',
-//         headerDetail: '',
-//         mediaType: '',
-//         mediaUrl: '',
-//         subheaders: [
-//             {
-//                 subheader: '',
-//                 subHeaderDetail: '',
-//                 mediaType: '',
-//                 mediaUrl: '',
-//             },
-//             {
-//                 subheader: '',
-//                 subHeaderDetail: '',
-//             },
-//         ],
-//     },
-
-//     testing: {
-//         header: '',
-//         headerDetail: '',
-//         subheaders: [
-//             {
-//                 subheader: '',
-//                 subHeaderDetail: '',
-//             },
-//             {
-//                 subheader: '',
-//                 subHeaderDetail: '',
-//             },
-//         ],
-//     },
-
-//     final: {
-//         header: '',
-//         headerDetail: '',
-//         mediaType: '',
-//         mediaUrl: '',
-//         subheaders: [
-//             {
-//                 subheader: '',
-//                 subHeaderDetail: '',
-//             },
-//             {
-//                 subheader: '',
-//                 subHeaderDetail: '',
-//             },
-//         ],
-//     },
-
-//     development: {
-//         header: '',
-//         headerDetail: '',
-//         subheaders: [
-//             {
-//                 subheader: '',
-//                 subHeaderDetail: '',
-//             },
-//             {
-//                 subheader: '',
-//                 subHeaderDetail: '',
-//             },
-//         ],
-//     },
-
-//     future: {
-//         header: '',
-//         headerDetail: '',
-//         subheaders: [],
-//     },
-// },
+// 	"poster": "",
+// 	"lgPoster": "",
+// 	"image": [
+// 	  { "image": "" },
+// 	  { "image": "" },
+// 	  { "image": "" },
+// 	  { "image": "" }
+// 	],
+// 	"colors": {
+// 	  "primary": "",
+// 	  "secondary": "",
+// 	  "ctaButton": "",
+// 	  "ctaButtonText": ""
+// 	},
+// 	"seo": "",
+// 	"name": "",
+// 	"shortDescription": "",
+// 	"timeline": "",
+// 	"title": "",
+// 	"teaser": "",
+// 	"price": "",
+// 	"intro": "",
+// 	"role": "",
+// 	"hasVideo": false,
+// 	"videoPath": "",
+// 	"problem": {
+// 	  "header": "",
+// 	  "headerDetail": "",
+// 	  "subheaders": [
+// 		{
+// 		  "subheader": "",
+// 		  "subHeaderDetail": ""
+// 		},
+// 		{
+// 		  "subheader": "",
+// 		  "subHeaderDetail": ""
+// 		}
+// 	  ]
+// 	},
+// 	"goal": {
+// 	  "header": "",
+// 	  "headerDetail": "",
+// 	  "subheaders": [
+// 		{
+// 		  "subheader": "",
+// 		  "subHeaderDetail": ""
+// 		},
+// 		{
+// 		  "subheader": "",
+// 		  "subHeaderDetail": ""
+// 		},
+// 		{
+// 		  "subheader": "",
+// 		  "subHeaderDetail": ""
+// 		}
+// 	  ]
+// 	},
+// 	"impact": {
+// 	  "header": "",
+// 	  "headerDetail": "",
+// 	  "subheaders": [
+// 		{
+// 		  "subheader": "",
+// 		  "subHeaderDetail": ""
+// 		},
+// 		{
+// 		  "subheader": "",
+// 		  "subHeaderDetail": ""
+// 		}
+// 	  ]
+// 	},
+// 	"ideation": {
+// 	  "header": "",
+// 	  "headerDetail": "",
+// 	  "mediaType": "",
+// 	  "mediaUrl": "",
+// 	  "subheaders": [
+// 		{
+// 		  "subheader": "",
+// 		  "subHeaderDetail": "",
+// 		  "mediaType": "",
+// 		  "mediaUrl": ""
+// 		},
+// 		{
+// 		  "subheader": "",
+// 		  "subHeaderDetail": ""
+// 		}
+// 	  ]
+// 	},
+// 	"testing": {
+// 	  "header": "",
+// 	  "headerDetail": "",
+// 	  "subheaders": [
+// 		{
+// 		  "subheader": "",
+// 		  "subHeaderDetail": ""
+// 		},
+// 		{
+// 		  "subheader": "",
+// 		  "subHeaderDetail": ""
+// 		}
+// 	  ]
+// 	},
+// 	"final": {
+// 	  "header": "",
+// 	  "headerDetail": "",
+// 	  "mediaType": "",
+// 	  "mediaUrl": "",
+// 	  "subheaders": [
+// 		{
+// 		  "subheader": "",
+// 		  "subHeaderDetail": ""
+// 		},
+// 		{
+// 		  "subheader": "",
+// 		  "subHeaderDetail": ""
+// 		}
+// 	  ]
+// 	},
+// 	"development": {
+// 	  "header": "",
+// 	  "headerDetail": "",
+// 	  "subheaders": [
+// 		{
+// 		  "subheader": "",
+// 		  "subHeaderDetail": ""
+// 		},
+// 		{
+// 		  "subheader": "",
+// 		  "subHeaderDetail": ""
+// 		}
+// 	  ]
+// 	},
+// 	"future": {
+// 	  "header": "",
+// 	  "headerDetail": "",
+// 	  "subheaders": []
+// 	}
+//   }
