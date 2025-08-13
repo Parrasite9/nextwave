00e18977c081afe6015d689c241a9a8ecd415926
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2025-08-13 11:35:31 -0500

    All pages now scroll to the top

--- src/Globals/SharedLayout.jsx
+++ src/Globals/SharedLayout.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+// src/Layout/SharedLayout.jsx
+import React, { useEffect } from 'react';
 import { Navbar, Footer } from '../Components/index';
+import { useLocation } from 'react-router-dom';
 
 const SharedLayout = ({ children }) => {
+	const { pathname } = useLocation();
+
+	useEffect(() => {
+		// Always scroll to top on route change
+		window.scrollTo({
+			top: 0,
+			left: 0,
+			behavior: 'smooth', // or 'auto' if you want instant
+		});
+	}, [pathname]);
+
 	return (
 		<div>
 			<Navbar />
-			{/* className='px-4 lg:px-10' */}
-			<main className="">{children}</main>
+			<main>{children}</main>
 			<Footer />
 		</div>
 	);
